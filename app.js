2a9d019727c1d111b57246ec6d1c5aa0fc3e662d
Sergeich3 <izsergeev2@gmail.com>
2022-11-17 20:42:23 +0300

    two commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// const userOne = {
+//     role: 'admin',
+//     firstname: 'Илья',
+// }
+
+
+
+// function checkAcccess(user,role){
+//     if(userOne.role === role){
+//         return true;
+//     }else {
+//         return false;
+//     }
+
+// }
+
+// function showSuccesMessage(user) {
+//     console.log(`доступ открыт ${user.firstname}`)
+// }
+
+// function showErrorMessage(user) {
+//     console.log(`доступ закрыт ${user.firstname}`)
+// }
+
+
+// if(checkAcccess(userOne, 'admin')){
+//     showSuccesMessage(userOne);
+// } else {
+//     showErrorMessage(userOne)
+// }
+
+
+//ключ это свойство объекта с помощью которого мы получаем данные
+
+//два способа чтобы получить данные из объекта; 1. через точку (userOne.role) 2. через квадратные скобки userOne['role']
+
+//функция-это блок кода для повторного использования любой части программы
+
+//
+
+
+
+// function getSqrt(num,step){
+//     return num ** step
+// }
+
+// console.log(getSqrt(100,2))
+
+
+// function getTest3(arg1,arg2,arg3){
+//     return (arg1 - arg2) / arg3
+// }
+// console.log (getTest3(100,50,2))
+
+// const args = {
+//     arg1: 100,
+//     arg2: 50,
+//     arg3: 2,
+// }
+
+// function getTest4(args){
+//     return (args.arg1 - args.arg2) / args.arg3
+// }
+// console.log (getTest4())
